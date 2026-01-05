4f54a9b53caf4ee891267880eca6a72c8855ccb2
shahzadahmad7489879 <shahzadahmad7489879@gmail.com>
2026-01-05 17:47:52 +0500

    Initial code

---
+++ app/ui/fonts.ts
@@ -0,0 +1,19 @@
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+
+import { Roboto } from 'next/font/google';
+
+export const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
